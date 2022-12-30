acabcc9af4318899d597e0f5844d84d238550635
PrPedrosa <prpedrosa95@gmail.com>
2022-12-30 17:57:06 +0000

    defines all logic

--- socket_server.js
+++ socket_server.js
@@ -2,7 +2,7 @@
 const app = require("./app")
 const http = require("http");
 const httpServer = http.createServer(app);
-httpServer.listen(8080, ()=> console.log("socket listening on port 8080"));
+httpServer.listen(9090, ()=> console.log("socket listening on port 9090"));
 
 const webSocketServer = require("websocket").server;
 
@@ -22,10 +22,14 @@ let randId = () => {
 
 let clients = []
 
+let clientsNames = []
+
 let messages = []
 
 module.exports = wsServer.on("request", request => {
     const connection = request.accept(null, request.origin);
+
+    if(clients.length > 10) connection.close()
 
     const clientId = randId();
     clients.push({
@@ -34,27 +38,27 @@ module.exports = wsServer.on("request", request => {
     })
     
     connection.on("connection", () =>  console.log("opened"))
+
     connection.on("close", () => {
         console.log(clients)
         clients = clients.filter(client => client.clientId !== clientId)
+        clientsNames = clientsNames.filter(client => client.clientId !== clientId)
         console.log(clients)
         console.log("closed!!")
         clearInterval(intervalId)
     })
-
 
     const payLoad = {
         "method": "connect",
         "clientId": clientId,
         "content": messages,
     }
-    
     connection.send(JSON.stringify(payLoad))
-    const intervalId = setInterval(() => {
 
+    const intervalId = setInterval(() => {
         const payLoad2 = {
             "method": "details",
-            "onlineNum": clients.length
+            "onlineClients": clientsNames 
         }
         connection.send(JSON.stringify(payLoad2))
     }, 3000);
@@ -63,29 +67,37 @@ module.exports = wsServer.on("request", request => {
         const msg = JSON.parse(data.utf8Data)
         
         if(msg.method === "newMessage") {
+            if(messages.length > 10){
+                messages.shift()
+            }
             messages.push({
                 "clientId": msg.clientId,
-                "content": msg.content
+                "content": msg.content,
+                "messageId": msg.messageId,
+                "date": msg.date
             })
 
             const payLoad = {
                 "method": "sendMessages",
                 "content": messages
             }
-
             clients.forEach(client => client.connection.send(JSON.stringify(payLoad)))
         }
 
         if(msg.method === "deleteMessage") {
-
-            messages = messages.filter(message => message.content !== msg.content && message.clientId !== msg.clientId)
-
+            messages = messages.filter(message => message.messageId !== msg.messageId)
             const payLoad = {
                 "method": "sendMessages",
                 "content": messages
             }
-
             clients.forEach(client => client.connection.send(JSON.stringify(payLoad)))
+        }
+
+        if(msg.method === "setName"){
+            clientsNames.push({
+                "clientId": msg.clientId,
+                "name": msg.clientName
+            })
         }
 
     })
